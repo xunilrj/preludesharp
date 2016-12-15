290fa358dbaeb1f793ad0111cbfec10f2b606e0e
Daniel Leite <xunilrj@hotmal.com>
2016-12-15 21:05:55 +0000

    first high order functions as objs

---
+++ PreludeSharp/PreludeSharp.Tests/PreludeSharpTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PreludeSharp;
+using System.Linq;
+
+namespace PreludeSharp.Tests
+{
+    [TestClass]
+    public class PreludeSharpTests
+    {
+        [TestMethod]
+        public void AllTests()
+        {
+            var list1 = new[] { 1, 2, 3 };
+
+            var sum1 = new Sum1();
+            var list2 = new Map<int, int>().Invoke(sum1, list1).ToArray();
+
+            Assert.AreEqual(2, list2[0]);
+            Assert.AreEqual(3, list2[1]);
+            Assert.AreEqual(4, list2[2]);
+
+            var zippedList = new Zip<int, int>().Invoke(list1, list2).ToArray();
+
+            Assert.AreEqual(1, zippedList[0].Item1);
+            Assert.AreEqual(2, zippedList[0].Item2);
+
+            var foldrSum = new Foldr<int, int>().Invoke(new Sum(), 0, list1);
+            Assert.AreEqual(6, foldrSum);
+
+            var foldlSum = new Foldl<int, int>().Invoke(new Sum(), 0, list1);
+            Assert.AreEqual(6, foldlSum);
+
+            var concatList = new Concat<int>().Invoke(new[] { list1, list2 }).ToArray();
+            Assert.AreEqual(6, concatList.Count());
+            Assert.AreEqual(1, concatList[0]);
+            Assert.AreEqual(4, concatList[5]);
+
+            var reversedConcatList = new Reverse<int>().Invoke(concatList).ToList();
+            Assert.AreEqual(4, reversedConcatList[0]);
+            Assert.AreEqual(1, reversedConcatList[5]);
+        }
+    }
+
+    public class Sum1 : IFunc<int, int>
+    {
+        public int Invoke(int item)
+        {
+            return item + 1;
+        }
+    }
+
+    public class Sum : IFunc<int, int, int>
+    {
+        public int Invoke(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ PreludeSharp/PreludeSharp/Prelude.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PreludeSharp
+{
+    public interface IAction { void Invoke(); }
+    public interface IAction<in T1> { void Invoke(T1 a); }
+    public interface IAction<in T1, in T2> { void Invoke(T1 a, T2 b); }
+    public interface IAction<in T1, in T2, in T3> { void Invoke(T1 a, T2 b, T3 c); }
+    public interface IAction<in T1, in T2, in T3, in T4> { void Invoke(T1 a, T2 b, T3 c, T4 d); }
+    public interface IAction<in T1, in T2, in T3, in T4, in T5> { void Invoke(T1 a, T2 b, T3 c, T4 d, T5 e); }
+
+    public interface IFunc<out TR> { TR Invoke(); }
+    public interface IFunc<in T1, out TR> { TR Invoke(T1 a); }
+    public interface IFunc<in T1, in T2, out TR> { TR Invoke(T1 a, T2 b); }
+    public interface IFunc<in T1, in T2, in T3, out TR> { TR Invoke(T1 a, T2 b, T3 c); }
+
+    public class Map<TA, TB> : IFunc<IFunc<TA, TB>, IEnumerable<TA>, IEnumerable<TB>>
+    {
+        public IEnumerable<TB> Invoke(IFunc<TA, TB> a, IEnumerable<TA> b)
+        {
+            foreach (var item in b)
+            {
+                yield return a.Invoke(item);
+            }
+        }
+    }
+
+
+    public class ZipWith<TA, TB, TC> : IFunc<IFunc<TA, TB, TC>, IEnumerable<TA>, IEnumerable<TB>, IEnumerable<TC>>
+    {
+        public IEnumerable<TC> Invoke(IFunc<TA, TB, TC> a, IEnumerable<TA> b, IEnumerable<TB> c)
+        {
+            return b.Zip(c, (bb, cc) => a.Invoke(bb, cc));
+        }
+    }
+
+    public class TupleCreation<TA, TB> : IFunc<TA, TB, Tuple<TA, TB>>
+    {
+        public Tuple<TA, TB> Invoke(TA a, TB b)
+        {
+            return Tuple.Create(a, b);
+        }
+    }
+
+    public class Zip<TA, TB> : IFunc<IEnumerable<TA>, IEnumerable<TB>, IEnumerable<Tuple<TA, TB>>>
+    {
+        public IEnumerable<Tuple<TA, TB>> Invoke(IEnumerable<TA> b, IEnumerable<TB> c)
+        {
+            return new ZipWith<TA, TB, Tuple<TA, TB>>().Invoke(new TupleCreation<TA, TB>(), b, c);
+        }
+    }
+
+    public class Foldr<TA, TB> : IFunc<IFunc<TA, TB, TB>, TB, IEnumerable<TA>, TB>
+    {
+        public TB Invoke(IFunc<TA, TB, TB> a, TB b, IEnumerable<TA> c)
+        {
+            var stack = new Stack<TA>(c);
+
+            var accum = b;
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                accum = a.Invoke(current, accum);
+            }
+
+            return accum;
+        }
+    }
+
+    public class Foldl<TA, TB> : IFunc<IFunc<TB, TA, TB>, TB, IEnumerable<TA>, TB>
+    {
+        public TB Invoke(IFunc<TB, TA, TB> a, TB b, IEnumerable<TA> c)
+        {
+            var stack = new Stack<TA>(c);
+
+            var accum = b;
+
+            foreach (var item in c)
+            {
+                accum = a.Invoke(accum, item);
+            }
+
+            return accum;
+        }
+    }
+
+    public class PlusPlus<TA> : IFunc<IEnumerable<TA>, IEnumerable<TA>, IEnumerable<TA>>
+    {
+        public IEnumerable<TA> Invoke(IEnumerable<TA> a, IEnumerable<TA> b)
+        {
+            foreach (var item in a)
+            {
+                yield return item;
+            }
+
+            foreach (var item in b)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public class Concat<TA> : IFunc<IEnumerable<IEnumerable<TA>>, IEnumerable<TA>>
+    {
+        public IEnumerable<TA> Invoke(IEnumerable<IEnumerable<TA>> a)
+        {
+            return new Foldr<IEnumerable<TA>, IEnumerable<TA>>().Invoke(
+                new PlusPlus<TA>(), Enumerable.Empty<TA>(), a);
+        }
+    }
+
+    public class ReverseOperation<TA> : IFunc<IEnumerable<TA>, TA, IEnumerable<TA>>
+    {
+        public IEnumerable<TA> Invoke(IEnumerable<TA> a, TA b)
+        {
+            yield return b;
+
+            foreach (var item in a)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public class Reverse<TA> : IFunc<IEnumerable<TA>, IEnumerable<TA>>
+    {
+        public IEnumerable<TA> Invoke(IEnumerable<TA> a)
+        {
+            return new Foldl<TA, IEnumerable<TA>>().Invoke(
+                new ReverseOperation<TA>(), Enumerable.Empty<TA>(), a);
+        }
+    }
+}
